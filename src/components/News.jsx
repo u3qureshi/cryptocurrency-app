8b33436fb252bd438997ea71d88f3d8abc01ce7d
Usman <usman-qureshi@outlook.com>
2022-10-01 20:28:27 -0400

    Add Crypto news select bar, edit news query

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 
 import { useGetNewsQuery } from "../services/newsApi";
+import { useGetCryptosQuery } from "../services/cryptoAPI";
 
 import newsImg from "../images/news-img.png";
 
@@ -10,14 +11,34 @@ const { Text, Title } = Typography;
 const { Option } = Select;
 
 export default function News({ simplified }) {
+  const [currNewsCat, setCurrNewsCat] = useState("Cryptocurrency");
   const { data: news } = useGetNewsQuery({
-    newsCat: "Cryptocurrency",
+    newsCat: currNewsCat,
     count: simplified ? 6 : 21,
   });
+  const { data } = useGetCryptosQuery(100);
 
-  console.log(news);
   return (
     <Row gutter={[23, 23]}>
+      {!simplified && (
+        <Col span={23}>
+          <Select
+            showSearch
+            className="select-news-div"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setCurrNewsCat(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {news?.value.map((n, i) => (
         <Col xs={23} sm={12} lg={7} key={i}>
           <Card hoverable className="news-card-div">
